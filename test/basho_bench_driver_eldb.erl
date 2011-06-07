a45eb28673ee61bf6d5c55d6e43e9d2c67172414
Dave Smith <dizzyd@dizzyd.com>
2011-06-07 07:52:32 -0600

    Tweak basho_bench driver to take advantage of new key gens in latest basho_bench

--- test/basho_bench_driver_eldb.erl
+++ test/basho_bench_driver_eldb.erl
@@ -9,25 +9,24 @@
 %% ====================================================================
 
 new(_Id) ->
-    %% Make sure e_leveldb module is available
-    %% case code:which(e_leveldb) of
-    %%     non_existing ->
-    %%         ?FAIL_MSG("~s requires e_leveldb to be available on code path.\n",
-    %%                   [?MODULE]);
-    %%     _ ->
-    %%         ok
-    %% end,
-
-    %% Pull the e_leveldb_config key which has all the key/value pairs for the 
+    %% Pull the e_leveldb_config key which has all the key/value pairs for the
     %% engine -- stuff everything into the e_leveldb application namespace
     %% so that starting the app will pull it in.
     application:load(e_leveldb),
     Config = basho_bench_config:get(e_leveldb_config, []),
     [ok = application:set_env(e_leveldb, K, V) || {K, V} <- Config],
 
-    os:cmd("rm -rf /tmp/bb.test"),
+    WorkDir = basho_bench_config:get(eldb_work_dir, "/tmp/eldb.bb"),
+    case basho_bench_config:get(eldb_clear_work_dir, false) of
+        true ->
+            io:format("Clearing work dir: " ++ WorkDir ++ "\n"),
+            os:cmd("rm -rf " ++ WorkDir ++ "/*");
+        false ->
+            ok
+    end,
 
-    case e_leveldb:open("/tmp/bb.test", [{create_if_missing, true}]) of
+    case e_leveldb:open(WorkDir, [{create_if_missing, true},
+                                  {cache_size, 1024 * 1024 * 128}]) of
         {ok, Ref} ->
             {ok, Ref};
         {error, Reason} ->
@@ -37,8 +36,6 @@ new(_Id) ->
 
 run(get, KeyGen, _ValueGen, State) ->
     case e_leveldb:get(State, KeyGen(), []) of
-        not_found ->
-            {ok, State};
         {ok, _Value} ->
             {ok, State};
         {error, Reason} ->
@@ -46,15 +43,15 @@ run(get, KeyGen, _ValueGen, State) ->
     end;
 run(put, KeyGen, ValueGen, State) ->
     print_status(State, 1000),
-    case e_leveldb:put(State, fmt_key(KeyGen()), ValueGen(), []) of
+    case e_leveldb:put(State, KeyGen(), ValueGen(), []) of
         ok ->
             {ok, State};
         {error, Reason} ->
             {error, State, Reason}
     end;
 run(batch_put, KeyGen, ValueGen, State) ->
-    print_status(State, 10),
-    Batch = [{put, fmt_key(KeyGen()), ValueGen()} || _ <- lists:seq(1, 100)],
+    print_status(State, 100),
+    Batch = [{put, KeyGen(), ValueGen()} || _ <- lists:seq(1, 100)],
     case e_leveldb:write(State, Batch, []) of
         ok ->
             {ok, State};
@@ -62,9 +59,6 @@ run(batch_put, KeyGen, ValueGen, State) ->
             {error, State, Reason}
     end.
 
-
-fmt_key(K) ->
-    list_to_binary(lists:flatten(io_lib:format("~w", [K]))).
 
 print_status(Ref, Count) ->
     status_counter(Count, fun() ->
