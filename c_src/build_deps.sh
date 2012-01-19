5e1a4c578e7caf84e80417b59b50a97dba26f645
Dave Smith <dizzyd@dizzyd.com>
2012-01-19 14:49:48 -0700

    Add snappy support

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-VSN="c8c5866a86c8d4a3e80d8708d14a06776fb683d1"
+LEVELDB_VSN="c8c5866a86c8d4a3e80d8708d14a06776fb683d1"
+SNAPPY_VSN="1.0.4"
 
 set -e
 
@@ -12,10 +13,20 @@ BASEDIR="$PWD"
 
 case "$1" in
     clean)
-        rm -rf leveldb
+        rm -rf leveldb system snappy-$SNAPPY_VSN
         ;;
 
     *)
+        if [ ! -d snappy-$SNAPPY_VSN ]; then
+            tar -xjf snappy-$SNAPPY_VSN.tar.gz
+            (cd snappy-$SNAPPY_VSN && ./configure --prefix=$BASEDIR/system)
+        fi
+
+        (cd snappy-$SNAPPY_VSN && make && make install)
+
+        export CFLAGS="$CFLAGS -I $BASEDIR/system/include"
+        export LDFLAGS="$LDFLAGS -L $BASEDIR/system/lib"
+
         if [ ! -d leveldb ]; then
             tar -xjf leveldb.tar.bz2
             (cd leveldb && git checkout $VSN)
