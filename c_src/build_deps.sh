a82ee271ce7dad7c4a2176b1160d7181f6ecd16b
Jon Meredith <jmeredith@basho.com>
2012-07-18 18:57:57 -0600

    Bumped version to 1.2.1 and updated leveldb for BLOCK.bad commits.

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -8,9 +8,10 @@ if [ `uname -s` = 'SunOS' -a "${POSIX_SHELL}" != "true" ]; then
 fi
 unset POSIX_SHELL # clear it so if we invoke other scripts, they run as ksh as well
 
-LEVELDB_VSN="fbe66f44e9abeb4f2de3c89d91573b7879226e6c" # June 25 merge w/ mv-write-throttle2
+LEVELDB_VSN="e81f2ff31ec055698e633ca1e8488691c05d0c56" # June 25 merge w/ mv-write-throttle2
                                                        # June 29 merge w/ mv-compress-msg for
                                                        # compression LOG fix
+                                                       # July 18 merge w/ BLOCK.bad fixes
 SNAPPY_VSN="1.0.4"
 
 set -e
