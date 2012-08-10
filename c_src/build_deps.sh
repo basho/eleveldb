5815f9ac5c723a8a1ece9ad7c931316e0d84e4de
Jon Meredith <jmeredith@basho.com>
2012-08-10 16:23:18 -0600

    Updated leveldb version for repair limiting.

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -8,13 +8,14 @@ if [ `uname -s` = 'SunOS' -a "${POSIX_SHELL}" != "true" ]; then
 fi
 unset POSIX_SHELL # clear it so if we invoke other scripts, they run as ksh as well
 
-LEVELDB_VSN="2aebdd9173a7840f9307e30146ac95f49fbe8e64" # June 25 merge w/ mv-write-throttle2
+LEVELDB_VSN="e30fb9eb674367f4d0ea801d18fc8f6b45fe0455" # June 25 merge w/ mv-write-throttle2
                                                        # June 29 merge w/ mv-compress-msg for
                                                        # compression LOG fix
                                                        # July 18 merge w/ BLOCK.bad fixes
                                                        # July 30 merge w/ cache lru fixes
                                                        # Aug  01 merge w/ LRUCache destructor
                                                        #         fixes
+                                                       # Aug  10 merge w/ L0 numfiles limit
 SNAPPY_VSN="1.0.4"
 
 set -e
