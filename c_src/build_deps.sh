cb28cab6bd518451acfc9eb67910a2b8b549b310
Matthew V <matthewv@basho.com>
2012-09-27 09:07:05 -0400

    Restore Davids patch for 3.2 / older compilers on Solaris and OSX

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -8,7 +8,7 @@ if [ `uname -s` = 'SunOS' -a "${POSIX_SHELL}" != "true" ]; then
 fi
 unset POSIX_SHELL # clear it so if we invoke other scripts, they run as ksh as well
 
-LEVELDB_VSN="309292405d514f7d6aed8bec6035ce30750395e7" # June 25 merge w/ mv-write-throttle2
+LEVELDB_VSN="0b174850575579def32357d6fc7d6b9feb80294e" # June 25 merge w/ mv-write-throttle2
                                                        # June 29 merge w/ mv-compress-msg for
                                                        # compression LOG fix
                                                        # July 18 merge w/ BLOCK.bad fixes
