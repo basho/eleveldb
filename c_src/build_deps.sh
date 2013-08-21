8278febd72c25b84d2fb1b41244861e47db106ac
Jared Morrow <jared@basho.com>
2013-08-20 20:02:51 -0600

    Bump leveldb dep to 1.3.3 and roll eLevelDB vsn

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -8,7 +8,7 @@ if [ `uname -s` = 'SunOS' -a "${POSIX_SHELL}" != "true" ]; then
 fi
 unset POSIX_SHELL # clear it so if we invoke other scripts, they run as ksh as well
 
-LEVELDB_VSN="1.3.2"
+LEVELDB_VSN="1.3.3"
 
 SNAPPY_VSN="1.0.4"
 
