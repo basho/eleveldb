4980a133a1aff206cf2b50acb6b3fa1481100964
Jared Morrow <jared@basho.com>
2013-12-11 09:15:57 -0700

    Update leveldb dep to 1.4.4

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -8,7 +8,7 @@ if [ `uname -s` = 'SunOS' -a "${POSIX_SHELL}" != "true" ]; then
 fi
 unset POSIX_SHELL # clear it so if we invoke other scripts, they run as ksh as well
 
-LEVELDB_VSN="72066610b8f78e042091d762606c4fdca431c028"
+LEVELDB_VSN="1.4.4"
 
 SNAPPY_VSN="1.0.4"
 
