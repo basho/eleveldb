434ab63e053f09f1481b8c5f1109db83ba08eb34
erikleitch <eleitch@basho.com>
2015-10-16 11:27:39 -0700

    Removed duplicate BytewiseComparatorImpl implementation from eleveldb

--- c_src/comparator.cc
+++ c_src/comparator.cc
@@ -13,56 +13,6 @@ using namespace leveldb;
 namespace leveldb {
 
 namespace {
-class BytewiseComparatorImpl : public Comparator {
- public:
-  BytewiseComparatorImpl() { }
-
-  virtual const char* Name() const {
-    return "leveldb.BytewiseComparator";
-  }
-
-  virtual int Compare(const Slice& a, const Slice& b) const {
-    return a.compare(b);
-  }
-
-  virtual void FindShortestSeparator(
-      std::string* start,
-      const Slice& limit) const {
-    // Find length of common prefix
-    size_t min_length = std::min(start->size(), limit.size());
-    size_t diff_index = 0;
-    while ((diff_index < min_length) &&
-           ((*start)[diff_index] == limit[diff_index])) {
-      diff_index++;
-    }
-
-    if (diff_index >= min_length) {
-      // Do not shorten if one string is a prefix of the other
-    } else {
-      uint8_t diff_byte = static_cast<uint8_t>((*start)[diff_index]);
-      if (diff_byte < static_cast<uint8_t>(0xff) &&
-          diff_byte + 1 < static_cast<uint8_t>(limit[diff_index])) {
-        (*start)[diff_index]++;
-        start->resize(diff_index + 1);
-        assert(Compare(*start, limit) < 0);
-      }
-    }
-  }
-
-  virtual void FindShortSuccessor(std::string* key) const {
-    // Find first character that can be incremented
-    size_t n = key->size();
-    for (size_t i = 0; i < n; i++) {
-      const uint8_t byte = (*key)[i];
-      if (byte != static_cast<uint8_t>(0xff)) {
-        (*key)[i] = byte + 1;
-        key->resize(i+1);
-        return;
-      }
-    }
-    // *key is a run of 0xffs.  Leave it alone.
-  }
-};
 
 // Time series key comparator.
 // Assumes first 8 bytes are a little endian 64 bit unsigned integer,
@@ -143,12 +93,20 @@ class TSComparator : public Comparator {
 
 }  // namespace
 
-static const BytewiseComparatorImpl bytewiseComparator;
 static const TSComparator           tsComparator;
 
+//------------------------------------------------------------
+// GetBytewiseComparator just returns leveldb's default bytewise
+// comparator
+//------------------------------------------------------------
+
 const Comparator* GetBytewiseComparator() {
-  return &bytewiseComparator;
+    return leveldb::BytewiseComparator();
 }
+
+//------------------------------------------------------------
+// GetTSComparator returns this module's internal TSComparator object
+//------------------------------------------------------------
 
 const Comparator* GetTSComparator() {
     return &tsComparator;
