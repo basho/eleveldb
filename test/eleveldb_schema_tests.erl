ee3de5c1482261287af65998d9e36e061e6fd7f6
Matthew V <matthewv@basho.com>
2016-09-07 10:14:06 -0400

    snappy as default if nothing in riak.conf, default riak.conf sets lz4

--- test/eleveldb_schema_tests.erl
+++ test/eleveldb_schema_tests.erl
@@ -134,7 +134,7 @@ compression_schema_test() ->
     Config2 = cuttlefish_unit:generate_templated_config(
         ["../priv/eleveldb.schema"], Case2, context(), predefined_schema()),
 
-    cuttlefish_unit:assert_config(Config2, "eleveldb.compression", lz4),
+    cuttlefish_unit:assert_config(Config2, "eleveldb.compression", snappy),
 
 
     %% Case3:  compression enabled, explicitly set lz4 as algorithm
@@ -157,6 +157,23 @@ compression_schema_test() ->
         ["../priv/eleveldb.schema"], Case4, context(), predefined_schema()),
 
     cuttlefish_unit:assert_config(Config4, "eleveldb.compression", snappy),
+
+    %% Case5:  compression enabled by default, explicitly set lz4 as algorithm
+    Case5 = [
+            {["leveldb", "compression", "algorithm"], lz4}
+           ],
+    Config5 = cuttlefish_unit:generate_templated_config(
+        ["../priv/eleveldb.schema"], Case5, context(), predefined_schema()),
+
+    cuttlefish_unit:assert_config(Config5, "eleveldb.compression", lz4),
+
+    %% Case6:  compression enabled by default, snappy by default
+    Case6 = [
+           ],
+    Config6 = cuttlefish_unit:generate_templated_config(
+        ["../priv/eleveldb.schema"], Case6, context(), predefined_schema()),
+
+    cuttlefish_unit:assert_config(Config6, "eleveldb.compression", snappy),
 
 
     ok.
