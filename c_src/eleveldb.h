ee445058d623d4e9b117668b279568ed9b7ee221
erikleitch <eleitch@basho.com>
2016-10-20 16:39:12 -0700

    Removed debugging method from eleveldb.h

--- c_src/eleveldb.h
+++ c_src/eleveldb.h
@@ -57,9 +57,6 @@ ERL_NIF_TERM streaming_ack(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
 ERL_NIF_TERM streaming_stop(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
 
 ERL_NIF_TERM currentMicroSeconds(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
-
-ERL_NIF_TERM decodeei(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
-
 } // namespace eleveldb
 
 
