b1033f7efbb5bc11052aeb2c2fb76987cf05ff6c
Matthew V <matthewv@basho.com>
2017-03-03 12:29:06 -0500

    add remove_metadata_pid()

--- c_src/router.h
+++ c_src/router.h
@@ -37,6 +37,7 @@ bool leveldb_callback(leveldb::EleveldbRouterActions_t, int , const void **);
 
 ERL_NIF_TERM property_cache(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
 ERL_NIF_TERM set_metadata_pid(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
+ERL_NIF_TERM remove_metadata_pid(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
 
 
 struct ServiceCallback
