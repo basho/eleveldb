e4a7c97c25e1009066f24798757d76b165c4d0d2
Andrei Zavada <andrei.zavada@tiot.jp>
2021-11-18 00:23:44 +0200

    ease graviton2 build

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -8,7 +8,7 @@ if [ `uname -s` = 'SunOS' -a "${POSIX_SHELL}" != "true" ]; then
 fi
 unset POSIX_SHELL # clear it so if we invoke other scripts, they run as ksh as well
 
-LEVELDB_VSN="2.0.36"
+LEVELDB_VSN="2.0.36-graviton2"
 
 SNAPPY_VSN="1.0.4"
 
@@ -21,6 +21,14 @@ if [ `basename $PWD` != "c_src" ]; then
 fi
 
 BASEDIR="$PWD"
+
+case `uname -m` in
+    aarch64)
+        if [ `uname` = "Linux" ]; then
+            CFLAGS="$CFLAGS -D__aarch64__"
+            CXXFLAGS="$CXXFLAGS -D__aarch64__"
+        fi
+esac
 
 # detecting gmake and if exists use it
 # if not use make
@@ -56,7 +64,7 @@ case "$1" in
 
     get-deps)
         if [ ! -d leveldb ]; then
-            git clone https://github.com/basho/leveldb
+            git clone https://github.com/TI-Tokyo/leveldb
             (cd leveldb && git checkout $LEVELDB_VSN)
             (cd leveldb && git submodule update --init)
         fi
@@ -76,7 +84,8 @@ case "$1" in
         
         if [ ! -d snappy-$SNAPPY_VSN ]; then
             tar -xzf snappy-$SNAPPY_VSN.tar.gz
-            (cd snappy-$SNAPPY_VSN && ./configure --disable-shared --prefix=$BASEDIR/system --libdir=$BASEDIR/system/lib --with-pic)
+            (cd snappy-$SNAPPY_VSN && ./configure --disable-shared --prefix=$BASEDIR/system --libdir=$BASEDIR/system/lib --with-pic \
+                                                  --build=aarch64-unknown-linux-gnu --host=aarch64-unknown-linux-gnu)
         fi
 
         if [ ! -f system/lib/libsnappy.a ]; then
@@ -89,7 +98,7 @@ case "$1" in
         export LEVELDB_VSN="$LEVELDB_VSN"
 
         if [ ! -d leveldb ]; then
-            git clone https://github.com/basho/leveldb
+            git clone https://github.com/TI-Tokyo/leveldb
             (cd leveldb && git checkout $LEVELDB_VSN)
             (cd leveldb && git submodule update --init)
         fi
